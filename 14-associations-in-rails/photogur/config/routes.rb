9d787563656ff2c52325f10821c840afb66ad9c2
NatalieBlack <natalievictoriablack@gmail.com>
2016-04-11 20:58:06 -0400

    pre-class code for associations lecture

---
+++ 14-associations-in-rails/photogur/config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+
+  root 'pictures#index'
+  resources :pictures
+
+end
---
+++ 14-associations-in-rails/photogur/db/seeds.rb
@@ -0,0 +1,10 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+Picture.create!(title: "Bunny", description: "Just look at this bunny", url: "http://theartmad.com/wp-content/uploads/2015/03/Bunny-2.jpg")
+Picture.create!(title: "Bird", description: "Standing on a branch and yelling", url: "http://40.media.tumblr.com/e0496fd38f282d7996bdce8618e6d2a5/tumblr_n7fwydGtMF1tu0z5no1_500.jpg")
+Picture.create!(title: "Guinea Pig", description: "A pepper for a hat", url: "http://cdn.cutestpaw.com/wp-content/uploads/2013/04/Guinea-pig-with-a-pepper-hat..jpg")
