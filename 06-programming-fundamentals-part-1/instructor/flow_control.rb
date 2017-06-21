56b1b9e20717aa9bfbe2184138e03b4345cf99ec
Mina Mikhail <mina@fightingtheboss.com>
2017-06-21 10:56:00 -0400

    Update 06 Programming Fundamentals Pt 1

---
+++ 06-programming-fundamentals-part-1/instructor/flow_control.rb
@@ -0,0 +1,20 @@
+# if statements
+
+# if (some condition is true)
+#   then do something awesome
+# (optionally) otherwise
+#   then do something else
+
+
+
+
+
+# Looping
+
+# for loop
+# while loop
+# until loop
+
+
+
+# Iteration
