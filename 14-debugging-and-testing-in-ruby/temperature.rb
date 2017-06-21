94c40c2dfc71c14d076cddac56486be913347640
NatalieBlack <natalievictoriablack@gmail.com>
2017-06-21 18:21:59 -0400

    rename lessons

--- 14-debugging-and-testing-in-ruby/temperature.rb
+++
@@ -1,3 +0,0 @@
-# This is where the method we're testing will go
-#  (T(°F) - 32) × 5/9
-#  T(°C) × 9/5 + 32
--- 14-debugging-and-testing-in-ruby/test_temperature.rb
+++
@@ -1,7 +0,0 @@
-require 'minitest/autorun'
-require 'minitest/pride'
-require_relative 'temperature.rb'
-
-class TestTemperature < MiniTest::Test
-   # tests go in here
-end
