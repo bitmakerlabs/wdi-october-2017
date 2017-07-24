de5cd3c4623ded4a6d17d184358d704dc649c7b2
Fred Ngo <fredngo@gmail.com>
2017-07-24 15:41:34 -0400

    Clean up 29-params-security

--- 29-params-security/seatyourself/app/models/user.rb
+++
@@ -1,5 +0,0 @@
-class User < ApplicationRecord
-  has_secure_password
-
-  validates :email, presence: true
-end
--- 29-params-security/seatyourself/db/migrate/20170619153350_add_credits_to_user.rb
+++
@@ -1,6 +0,0 @@
-class AddCreditsToUser < ActiveRecord::Migration[5.1]
-  def change
-    add_column :users, :credits, :integer
-    User.update_all(credits: 0)
-  end
-end
