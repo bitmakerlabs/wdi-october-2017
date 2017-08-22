62ee10144b79c71f06576c7080e8391730387acb
Fred Ngo <fredngo@gmail.com>
2017-08-21 22:28:48 -0400

    [45 - AJAX and JSON] Upgrade robots app to Rails 5.1.3

--- 45-ajax-and-json/robots/config/routes.rb
+++ 45-ajax-and-json/robots/config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-
-  root 'robots#index'
-  resources :robots
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :robots
+  root 'robots#index'
 end
