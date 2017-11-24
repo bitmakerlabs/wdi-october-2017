88de1673834994718afc4e4e6f485de6a24646f2
Bitmaker Labs <bitmakerlabs@Bitmaker-2.local>
2017-11-24 10:26:49 -0500

    [25-migrations-and-validations] Example of class methods using where

--- 25-migrations-and-validations/instructor/chinook/app/models/track.rb
+++ 25-migrations-and-validations/instructor/chinook/app/models/track.rb
@@ -1,2 +1,26 @@
 class Track < ApplicationRecord
+
+  SHORT = 180000
+  LONG  = 360000
+
+  def self.short
+    shorter_than(SHORT)
+  end
+
+  def self.medium
+    longer_than_or_equal_to(SHORT).shorter_than(LONG)
+  end
+
+  def self.long
+    longer_than_or_equal_to(LONG)
+  end
+
+  def self.shorter_than(milliseconds)
+    where("milliseconds < ?", milliseconds)
+  end
+
+  def self.longer_than_or_equal_to(milliseconds)
+    where("milliseconds >= ?", milliseconds)
+  end
+
 end
