21b06843b40bc8d29d018b65123806b152a77293
Bitmaker Labs <bitmakerlabs@Bitmaker-2.local>
2017-11-24 11:16:47 -0500

    [25-migrations-and-validations] Add Ranking column to Tracks table

--- 25-migrations-and-validations/instructor/chinook/db/schema.rb
+++ 25-migrations-and-validations/instructor/chinook/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140128205156) do
+ActiveRecord::Schema.define(version: 20171124160819) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 20140128205156) do
     t.decimal "unit_price", precision: 10, scale: 2
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer "ranking"
   end
 
 end
