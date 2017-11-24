4e4a450c7d55bd0b423966b5ddc7cab160ed12a3
Bitmaker Labs <bitmakerlabs@Bitmaker-2.local>
2017-11-24 12:08:25 -0500

    [25-migrations-and-validations] Add validation examples

--- 25-migrations-and-validations/instructor/chinook/app/models/album.rb
+++ 25-migrations-and-validations/instructor/chinook/app/models/album.rb
@@ -1,2 +1,5 @@
 class Album < ApplicationRecord
+
+  validates :title, presence: true
+
 end
--- 25-migrations-and-validations/instructor/chinook/app/models/track.rb
+++ 25-migrations-and-validations/instructor/chinook/app/models/track.rb
@@ -3,6 +3,17 @@ class Track < ApplicationRecord
   SHORT = 180000
   LONG  = 360000
 
+  validates :name, :composer, :milliseconds, :bytes, :unit_price, presence: true
+  validates :milliseconds, :bytes, numericality: {
+                                     only_integer: true,
+                                     greater_than: 0
+                                   }
+  validates :unit_price, numericality: {
+                           greater_than_or_equal_to: 0.0
+                         }
+
+  validate :name_must_be_titleized
+
   scope :short, -> { shorter_than(SHORT) }
   scope :medium, -> {longer_than_or_equal_to(SHORT).shorter_than(LONG) }
   scope :long, -> { longer_than_or_equal_to(LONG) }
@@ -15,5 +26,21 @@ class Track < ApplicationRecord
   scope :longer_than_or_equal_to, -> (milliseconds) {
     where('milliseconds >= ?', milliseconds) if milliseconds && milliseconds > 0
   }
+
+private
+
+  def name_must_be_titleized
+    # Step 1: Make sure there is name
+    return unless name.present?
+
+    # Step 2: Check that the first character is an uppercase letter
+    first_char = name[0]
+
+    # 'a' != 'a'.upcase ===> 'a' != 'A'
+    first_char_is_not_upcased = (first_char != first_char.upcase)
+
+    # Step 3: Add an error if the first char is not uppercase
+    errors.add(:name, 'must be capitalized') if first_char_is_not_upcased
+  end
 
 end
