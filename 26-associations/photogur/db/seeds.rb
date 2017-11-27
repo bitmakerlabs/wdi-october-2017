66b9ab4f9ea24f82a64de4e0ae0f3f2a32b6d686
NatalieBlack <natalievictoriablack@gmail.com>
2017-11-27 12:09:43 -0500

    Add tag-related seeds for 26-associations

--- 26-associations/photogur/db/seeds.rb
+++ 26-associations/photogur/db/seeds.rb
@@ -9,3 +9,12 @@ Picture.destroy_all
 Picture.create!(title: "Bunny", description: "Just look at this bunny", url: "http://theartmad.com/wp-content/uploads/2015/03/Bunny-2.jpg")
 Picture.create!(title: "Bird", description: "Standing on a branch and yelling", url: "http://40.media.tumblr.com/e0496fd38f282d7996bdce8618e6d2a5/tumblr_n7fwydGtMF1tu0z5no1_500.jpg")
 Picture.create!(title: "Guinea Pig", description: "A pepper for a hat", url: "http://i.imgur.com/5bNMuVa.jpg")
+
+animal_tag = Tag.create!(name: "Animals")
+hat_tag = Tag.create!(name: "Hats")
+
+Picture.all.each do |pic|
+  pic.tags << animal_tag
+end
+
+Picture.last.tags << hat_tag
