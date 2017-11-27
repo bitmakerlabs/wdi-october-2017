e104301109fcc35a73aae9080a97f95a00481972
NatalieBlack <natalievictoriablack@gmail.com>
2017-11-27 09:56:32 -0500

    Update seeds for photogur app in 26-associations

--- 26-associations/photogur/db/seeds.rb
+++ 26-associations/photogur/db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Picture.destroy_all
 Picture.create!(title: "Bunny", description: "Just look at this bunny", url: "http://theartmad.com/wp-content/uploads/2015/03/Bunny-2.jpg")
 Picture.create!(title: "Bird", description: "Standing on a branch and yelling", url: "http://40.media.tumblr.com/e0496fd38f282d7996bdce8618e6d2a5/tumblr_n7fwydGtMF1tu0z5no1_500.jpg")
-Picture.create!(title: "Guinea Pig", description: "A pepper for a hat", url: "http://cdn.cutestpaw.com/wp-content/uploads/2013/04/Guinea-pig-with-a-pepper-hat..jpg")
+Picture.create!(title: "Guinea Pig", description: "A pepper for a hat", url: "http://i.imgur.com/5bNMuVa.jpg")
