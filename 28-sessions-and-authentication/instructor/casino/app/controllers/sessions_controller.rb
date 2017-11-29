bc8b4b3195c6b67427965e258bce1c21ee735330
Bitmaker Labs <bitmakerlabs@Bitmaker-2.local>
2017-11-29 12:04:12 -0500

    [28-sessions-and-authentication] Implement SessionsController#create

--- 28-sessions-and-authentication/instructor/casino/app/controllers/sessions_controller.rb
+++ 28-sessions-and-authentication/instructor/casino/app/controllers/sessions_controller.rb
@@ -3,6 +3,16 @@ class SessionsController < ApplicationController
   end
 
   def create
+    u = User.find_by(email: params[:session][:email])
+
+    if u && u.authenticate(params[:session][:password])
+      flash[:notice] = 'Login successful'
+      redirect_to root_url
+    else
+      flash[:error] = 'Access DENIED!'
+      render :new
+    end
+
   end
 
   def destroy
