88a9e7bcb976b74f91af5abfbde8547ecbbd0f8d
NatalieBlack <natalievictoriablack@gmail.com>
2017-12-07 10:54:01 -0500

    In-class code for first half of 31-how-to-test

--- 31-how-to-test/temperature.rb
+++ 31-how-to-test/temperature.rb
@@ -1,3 +1,11 @@
 # This is where the method we're testing will go
 #  (T(°F) - 32) × 5/9
 #  T(°C) × 9/5 + 32
+
+def f_to_c(f_temp)
+  (f_temp - 32) * 5.0/9.0
+end
+
+def c_to_f(c_temp)
+  c_temp * 9.0/5.0 + 32
+end
--- 31-how-to-test/test_temperature.rb
+++ 31-how-to-test/test_temperature.rb
@@ -4,4 +4,34 @@ require_relative 'temperature.rb'
 
 class TestTemperature < MiniTest::Test
    # tests go in here
+
+   def test_c_to_f_positive
+     expected_value = 50
+     actual_value = c_to_f(10)
+
+     assert_equal(expected_value, actual_value)
+   end
+
+   def test_c_to_f_negative
+     expected_value = -4
+     actual_value = c_to_f(-20)
+
+     assert_equal(expected_value, actual_value)
+   end
+
+   def test_f_to_c_positive
+     # 50 to 10
+     expected_value = 10
+     actual = f_to_c(50)
+
+     assert_equal(expected_value, actual)
+   end
+
+   def test_f_to_c_negative
+     # -4 is -20
+     expected_value = -20
+     actual_value = f_to_c(-4)
+
+     assert_equal(expected_value, actual_value)
+   end
 end
