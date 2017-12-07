e39ceea77e4d84a34b82881111b088f85f2517a7
NatalieBlack <natalievictoriablack@gmail.com>
2017-12-06 22:16:22 -0500

    Add skips to unit tests in seatyoursef for 31-how-to-test

--- 31-how-to-test/seatyourself/test/models/restaurant_test.rb
+++ 31-how-to-test/seatyourself/test/models/restaurant_test.rb
@@ -3,39 +3,51 @@ require 'test_helper'
 class RestaurantTest < ActiveSupport::TestCase
 
   def test_restaurant_is_invalid_without_name
+    skip
   end
 
   def test_restaurant_is_invalid_without_opening_hour
+    skip
   end
 
   def test_restaurant_is_invalid_without_closing_hour
+    skip
   end
 
   def test_restaurant_is_available_when_empty_during_opening_hours
+    skip
   end
 
   def test_restaurant_is_available_when_under_capacity_during_opening_hours
+    skip
   end
 
   def test_restaurant_is_available_later_same_day
+    skip
   end
 
   def test_restaurant_is_unavailable_in_the_past
+    skip
   end
 
   def test_restaurant_is_unavailable_when_full
+    skip
   end
 
   def test_restaurant_is_unavailable_before_opening
+    skip
   end
 
   def test_restaurant_is_available_at_opening
+    skip
   end
 
   def test_restaurant_is_unavailable_at_closing_time
+    skip
   end
 
   def test_restaurant_is_unavailable_past_closing_time
+    skip
   end
 
 end
