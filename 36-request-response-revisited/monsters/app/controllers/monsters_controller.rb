40161b04a81b0c5806d047c751420c9b2043d963
Mina Mikhail <mina@fightingtheboss.com>
2017-12-14 16:13:32 -0500

    Add in-class code for 36 Request Response Revisited

--- 36-request-response-revisited/monsters/app/controllers/monsters_controller.rb
+++ 36-request-response-revisited/monsters/app/controllers/monsters_controller.rb
@@ -2,17 +2,26 @@ class MonstersController < ApplicationController
   def index
     @monsters = Monster.all
 
+    # Headers
+    # Request
+    # Accepts: application/json, */*
+
+    # Response
+    # Content-Type: application/json, text/html, text/plain
+
     respond_to do |format|
       format.html
       format.text
       format.json do
-        render json: @monsters.map do |monster|
+        monster_array = @monsters.map do |monster|
           {
             full_name: monster.name,
             location: monster.home,
             creepiness: monster.creepiness
           }
         end
+
+        render json: monster_array
       end
     end
 
